eff4ca66674fafa65f135916cabfac3e7d4a29f5
katriyna <katriyna@gmail.com>
2018-09-10 17:10:00 +0300

    empty-widget: different font size for message and additional content with links and/or description. + Also import all smiles via importing one variable instead of importing each smile separately. [publish]

--- src/empty-widget/empty-widget.js
+++ src/empty-widget/empty-widget.js
@@ -3,20 +3,42 @@ import PropTypes from 'prop-types';
 
 import styles from './empty-widget.css';
 
-const EmptyWidget = ({header, children}) => (
+const EmptyWidgetSmiles = {
+  ERROR: '{{ (>_<) }}',
+  JOY: '(⌒‿⌒)',
+  OK: '(・_・)'
+};
+
+const EmptyWidget = ({smile, message, children}) => (
   <div className={styles.empty}>
-    <div className={styles.smile}>{header}</div>
-    <div className={styles.message}>{children}</div>
+    {
+      smile &&
+      <div className={styles.smile}>
+        {smile}
+      </div>
+    }
+    {
+      message &&
+      <div className={styles.message}>
+        {message}
+      </div>
+    }
+    {
+      children &&
+      <div className={styles.additionalContent}>
+        {children}
+      </div>
+    }
   </div>
 );
 
 EmptyWidget.propTypes = {
-  header: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired
+  smile: PropTypes.string,
+  message: PropTypes.string,
+  children: PropTypes.node
 };
 
-export const ERROR_FACE = '{{ (>_<) }}';
-export const JOY_FACE = '(⌒‿⌒)';
-export const OK_FACE = '(・_・)';
 
 export default EmptyWidget;
+
+export {EmptyWidgetSmiles};
