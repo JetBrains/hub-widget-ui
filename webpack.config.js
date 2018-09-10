0ac0c55afdd1ff73a521e2464f38e44bd474c6e4
katriyna <katriyna@gmail.com>
2018-09-10 13:51:22 +0300

    get rid of ExtractTextPlugin to be able to add styles with the same import-line as component itself

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 // const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ringUiWebpackConfig = require('@jetbrains/ring-ui/webpack.config');
 
 const pkg = require('./package.json');
@@ -20,52 +19,43 @@ module.exports = {
     publicPath: '/dist/',
     umdNamedDefine: true
   },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: '[name].css'
-    })
-  ],
   module: {
     rules: [
       ...ringUiWebpackConfig.config.module.rules,
       {
         test: /\.*css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                importLoaders: 1,
-                localIdentName: '[name]__[local]__[hash:base64:7]'
-              }
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: '[name]__[local]__[hash:base64:7]'
+          }
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            config: {
+              ctx: {variables: require('@jetbrains/ring-ui/extract-css-vars')}
             },
-            {
-              loader: 'postcss-loader',
-              options: {
-                config: {
-                  ctx: {variables: require('@jetbrains/ring-ui/extract-css-vars')}
-                },
-                plugins: () => [
-                  require('postcss-import')({}),
-                  require('postcss-modules-values-replace')({}),
-                  require('postcss-cssnext')({
-                    features: {
-                      calc: {
-                        mediaQueries: true
-                      },
-                      customProperties: {
-                        preserve: true,
-                        variables: require('@jetbrains/ring-ui/extract-css-vars')
-                      }
-                    }
-                  })
-                ]
-              }
-            }
-          ]
-        })
+            plugins: () => [
+              require('postcss-import')({}),
+              require('postcss-modules-values-replace')({}),
+              require('postcss-cssnext')({
+                features: {
+                  calc: {
+                    mediaQueries: true
+                  },
+                  customProperties: {
+                    preserve: true,
+                    variables: require('@jetbrains/ring-ui/extract-css-vars')
+                  }
+                }
+              })
+            ]
+          }
+        }]
       },
       {
         test: /\.js$/,
