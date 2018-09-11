255d0e643a081c10f26ee239752ee91e6bc8a199
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-09-11 20:21:24 +0300

    Move to @jetbrains/babel-preset-jetbrains

--- webpack.config.js
+++ webpack.config.js
@@ -76,11 +76,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              '@babel/preset-env',
-              '@babel/preset-react'
-            ],
-            plugins: [
-              '@babel/plugin-proposal-class-properties'
+              '@jetbrains/jetbrains'
             ]
           }
         },
